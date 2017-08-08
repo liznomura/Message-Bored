a0fc23d9d60f900f91066f93cb84f80552906f16
liznomura <lizn@hawaii.edu>
2017-08-07 19:22:50 -1000

    setup client

--- index.js
+++ index.js
@@ -2,18 +2,21 @@
 const sequelize = require('sequelize');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-
 const express = require('express');
 const app = express();
+
+const PORT = process.env.PORT || 8080;
 
 const db = require('./models');
 const Users = db.users;
 const Topics = db.topics;
 const Messages = db.messages;
 
-const PORT = process.env.PORT || 8080;
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   console.log('hello');
