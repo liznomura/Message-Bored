7a0e713d8212d1bf630c9a41242b6cc7742b53e8
liznomura <lizn@hawaii.edu>
2017-08-08 09:01:53 -1000

    required in stuff for users

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 /* jshint esversion:6 */
+const express = require('express');
+const session = require('express-session');
 const sequelize = require('sequelize');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const express = require('express');
 const app = express();
-
-const PORT = process.env.PORT || 8080;
 
 const db = require('./models');
 const Users = db.users;
 const Topics = db.topics;
 const Messages = db.messages;
 
+const PORT = process.env.PORT || 8080;
+
+const usersRouter = require('./routes/users');
+const topicsRouter = require('./routes/topics');
+const messagesRouter = require('./routes/messages');
 
 app.use(express.static('./public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+// app.use('/users', usersRouter);
+// app.use('/topics', topicsRouter);
+// app.use('/messages', messagesRouter);
 
 app.get('/', (req, res) => {
   console.log('hello');
